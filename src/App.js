b18cd631892c375589eb55b839ebce05f56d7f64
unknown <tatlampoon@gmail.com>
2023-06-23 11:48:46 +0800

    finish styling

--- src/App.js
+++ src/App.js
@@ -12,25 +12,25 @@ function App() {
           1231111111111111111111111111111111111
         </div>
       </div>
-      <button>AC</button>
-      <button>DEL</button>
-      <button>%</button>
-      <button>÷</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>-</button>
-      <button className="span-two">0</button>
-      <button>.</button>
-      <button>=</button>
+      <button className="topBtn">AC</button>
+      <button className="topBtn">DEL</button>
+      <button className="topBtn">%</button>
+      <button className="funcBtn">÷</button>
+      <button className="nums">7</button>
+      <button className="nums">8</button>
+      <button className="nums">9</button>
+      <button className="funcBtn">×</button>
+      <button className="nums">4</button>
+      <button className="nums">5</button>
+      <button className="nums">6</button>
+      <button className="funcBtn">+</button>
+      <button className="nums">1</button>
+      <button className="nums">2</button>
+      <button className="nums">3</button>
+      <button className="funcBtn">-</button>
+      <button className="span-two nums">0</button>
+      <button className="nums">.</button>
+      <button className="funcBtn">=</button>
     </div>
   );
 }
